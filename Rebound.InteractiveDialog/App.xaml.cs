3448bd584851bf61124cea07a3966c88fa4a8bbd
Lamparter <notlamparter@outlook.com>
2024-10-06 10:56:36 +0100

    Add restricted window definition to `Rebound.InteractiveDialog`

--- Rebound.InteractiveDialog/App.xaml.cs
+++ Rebound.InteractiveDialog/App.xaml.cs
@@ -29,6 +29,7 @@ public partial class App : Application
     /// Initializes the singleton application object.  This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
+
     public App()
     {
         this.InitializeComponent();
--- Rebound.InteractiveDialog/MainWindow.xaml.cs
+++ Rebound.InteractiveDialog/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,13 +12,25 @@ namespace Rebound.InteractiveDialog;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private AppWindow _apw;
+    private OverlappedPresenter _presenter;
+
+    public void GetAppWindowAndPresenter()
+    {
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+        _apw = AppWindow.GetFromWindowId(myWndId);
+        _presenter = _apw.Presenter as OverlappedPresenter;
+    }
     public MainWindow()
     {
         this.InitializeComponent();
-    }
 
-    private void myButton_Click(object sender, RoutedEventArgs e)
-    {
-        myButton.Content = "Clicked";
+        GetAppWindowAndPresenter();
+        _presenter.IsMaximizable = false;
+        _presenter.IsMinimizable = false;
+        this.AppWindow.MoveAndResize(new Windows.Graphics.RectInt32(548, 184, 548, 184));
+        this.SetIsMaximizable(false);
+        this.SetIsResizable(false);
     }
 }
