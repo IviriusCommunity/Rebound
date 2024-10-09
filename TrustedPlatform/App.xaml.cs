fa02297bbacfc98aa3086a7918c531c064c70165
Ivirius <zaitandreicristian@outlook.com>
2024-10-10 01:58:58 +0300

    Use title bar service

--- TrustedPlatform/App.xaml.cs
+++ TrustedPlatform/App.xaml.cs
@@ -14,7 +14,6 @@ public partial class App : Application
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         m_window = new MainWindow();
-        m_window.SetWindowSize(1100, 750);
         m_window.Show();
     }
 }
