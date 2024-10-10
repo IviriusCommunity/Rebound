14c626ce7ade518120660f9001ecfab55b3c2f21
Lamparter <notlamparter@outlook.com>
2024-10-10 18:40:26 +0100

    Add CPL project

---
+++ Control/MainWindow.xaml.cs
@@ -0,0 +1,262 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Rebound.Control.Views;
+using Rebound.Helpers;
+using Rebound.Control.Views;
+using Windows.System;
+using Rebound.Control;
+using WinUIEx;
+
+namespace Rebound.Control;
+
+public sealed partial class MainWindow : WindowEx
+{
+    public TitleBarService TitleBarEx;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        SystemBackdrop = new MicaBackdrop();
+        //AppWindow.DefaultTitleBarShouldMatchAppModeTheme = true;
+        Title = "Control Panel";
+        WindowTitle.Text = Title;
+        RootFrame.Navigate(typeof(ModernHomePage));
+        this.SetWindowSize(1250, 750);
+
+        Read();
+
+        AddressBox.Text = "Control Panel";
+        AddressBox.ItemsSource = new List<string>()
+        {
+            @"Control Panel",
+            @"Control Panel\Appearance and Personalization",
+            @"Control Panel\System and Security",
+        };
+        NavigateToPath();
+        RootFrame.BackStack.Clear();
+        RootFrame.ForwardStack.Clear();
+
+        TitleBarEx = new TitleBarService(this, AccentStrip, TitleBarIcon, WindowTitle, Close, CrimsonMaxRes, Minimize, MaxResGlyph, WindowContent);
+        TitleBarEx.SetWindowIcon("Assets\\AppIcons\\rcontrol.ico");
+
+        var rects = Display.GetDPIAwareDisplayRect(this);
+        if (rects.Height < 900 || rects.Width < 1200)
+        {
+            this.SetWindowSize(rects.Width - 200, rects.Height - 200);
+        }
+    }
+
+    private async void Read()
+    {
+        await Task.Delay(50);
+        try
+        {
+            BackButton.IsEnabled = RootFrame.CanGoBack;
+            ForwardButton.IsEnabled = RootFrame.CanGoForward;
+
+            Read();
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void BackButton_Click(object sender, RoutedEventArgs e)
+    {
+        RootFrame.GoBack();
+        AddressBox.Text = CurrentPage();
+        NavigateToPath();
+    }
+
+    private void ForwardButton_Click(object sender, RoutedEventArgs e)
+    {
+        RootFrame.GoForward();
+        AddressBox.Text = CurrentPage();
+        NavigateToPath();
+    }
+
+    private async void UpButton_Click(object sender, RoutedEventArgs e)
+    {
+        await Launcher.LaunchUriAsync(new Uri(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)));
+        App.cpanelWin.Close();
+    }
+
+    private void RefreshButton_Click(object sender, RoutedEventArgs e)
+    {
+        var oldHistory = RootFrame.ForwardStack;
+        var newList = new List<PageStackEntry>();
+        foreach (var item in oldHistory)
+        {
+            newList.Add(item);
+        }
+        RootFrame.Navigate(typeof(HomePage), null, new Microsoft.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
+        RootFrame.GoBack();
+        RootFrame.ForwardStack.Clear();
+        foreach (var item in newList)
+        {
+            RootFrame.ForwardStack.Add(item);
+        }
+        AddressBox.Text = CurrentPage();
+        NavigateToPath();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        AddressBox.Text = @"Control Panel";
+        NavigateToPath();
+    }
+
+    private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+    {
+        AddressBox.Text = @"Control Panel\Appearance and Personalization";
+        NavigateToPath();
+    }
+
+    private void MenuFlyoutItem_Click_1(object sender, RoutedEventArgs e)
+    {
+        AddressBox.Text = @"Control Panel\System and Security";
+        NavigateToPath();
+    }
+
+    private void WindowEx_Closed(object sender, WindowEventArgs args)
+    {
+        App.cpanelWin = null;
+    }
+
+    private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        AddressBar.Visibility = Visibility.Visible;
+        AddressBox.Visibility = Visibility.Collapsed;
+    }
+
+    private async void AddressBar_PointerReleased(object sender, PointerRoutedEventArgs e)
+    {
+        AddressBar.Visibility = Visibility.Collapsed;
+        AddressBox.Visibility = Visibility.Visible;
+        await Task.Delay(10);
+        AddressBox.Focus(FocusState.Programmatic);
+    }
+
+    public void HideAll()
+    {
+        AppearanceAndPersonalization.Visibility = Visibility.Collapsed;
+        SystemAndSecurity.Visibility = Visibility.Collapsed;
+        WindowsTools.Visibility = Visibility.Collapsed;
+    }
+
+    public string CurrentPage()
+    {
+        switch (RootFrame.Content)
+        {
+            case Rebound.Control.Views.AppearanceAndPersonalization:
+                {
+                    return @"Control Panel\Appearance and Personalization";
+                }
+            case Rebound.Control.Views.SystemAndSecurity:
+                {
+                    return @"Control Panel\System and Security";
+                }
+            case Rebound.Control.Views.WindowsTools:
+                {
+                    return @"Control Panel\System and Security\Windows Tools";
+                }
+            case ModernHomePage:
+                {
+                    return @"Control Panel";
+                }
+            case HomePage:
+                {
+                    return @"Control Panel";
+                }
+            default:
+                {
+                    return @"Control Panel";
+                }
+        }
+    }
+
+    private void AddressBox_KeyUp(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == VirtualKey.Enter)
+        {
+            NavigateToPath();
+        }
+    }
+
+    public async void NavigateToPath(bool legacyHomePage = false)
+    {
+        HideAll();
+        RootFrame.Focus(FocusState.Programmatic);
+        switch (AddressBox.Text)
+        {
+            case @"Control Panel\Appearance and Personalization":
+                {
+                    if (RootFrame.Content is not Rebound.Control.Views.AppearanceAndPersonalization)
+                    {
+                        App.cpanelWin.RootFrame.Navigate(typeof(AppearanceAndPersonalization), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+                    }
+                    AppearanceAndPersonalization.Visibility = Visibility.Visible;
+                    return;
+                }
+            case @"Control Panel\System and Security":
+                {
+                    if (RootFrame.Content is not Rebound.Control.Views.SystemAndSecurity)
+                    {
+                        App.cpanelWin.RootFrame.Navigate(typeof(SystemAndSecurity), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+                    }
+                    SystemAndSecurity.Visibility = Visibility.Visible;
+                    return;
+                }
+            case @"Control Panel\System and Security\Windows Tools":
+                {
+                    if (RootFrame.Content is not Rebound.Control.Views.WindowsTools)
+                    {
+                        App.cpanelWin.RootFrame.Navigate(typeof(WindowsTools), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+                    }
+                    SystemAndSecurity.Visibility = Visibility.Visible;
+                    WindowsTools.Visibility = Visibility.Visible;
+                    return;
+                }
+            case @"Control Panel":
+                {
+                    if (legacyHomePage == false && RootFrame.Content is not ModernHomePage)
+                    {
+                        App.cpanelWin.RootFrame.Navigate(typeof(ModernHomePage), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+                    }
+                    else if (legacyHomePage != false && RootFrame.Content is not HomePage)
+                    {
+                        App.cpanelWin.RootFrame.Navigate(typeof(HomePage), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+                    }
+                    return;
+                }
+            default:
+                {
+                    try
+                    {
+                        await Launcher.LaunchUriAsync(new Uri(AddressBox.Text));
+                        Close();
+                        return;
+                    }
+                    catch
+                    {
+                        AddressBox.Text = CurrentPage();
+                    }
+                    return;
+                }
+        }
+    }
+
+    private async void AddressBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+    {
+        await Task.Delay(10);
+        NavigateToPath();
+    }
+}
---
+++ Control/Views/ModernHomePage.xaml.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Diagnostics;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.System;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Rebound.Control.Views;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class ModernHomePage : Page
+{
+    public ModernHomePage()
+    {
+        this.InitializeComponent();
+        if (App.cpanelWin != null) App.cpanelWin.TitleBarEx.SetWindowIcon("Assets\\AppIcons\\rcontrol.ico");
+        if (App.cpanelWin != null) App.cpanelWin.Title = "Rebound Control Panel";
+        LoadWallpaper();
+        DisplaySystemInformation();
+    }
+
+    private void DisplaySystemInformation()
+    {
+        // Get Current User
+        string currentUser = Environment.UserName;
+
+        // Get PC Name
+        string pcName = Environment.MachineName;
+
+        // Get CPU Name
+        string cpuName = GetCPUName();
+
+        // Get RAM Capacity
+        string ramCapacity = GetRAMCapacity();
+
+        // Displaying the information (Assuming you have TextBlocks in your XAML)
+        CurrentUser.Text = $"Logged in as {currentUser}";
+        PCName.Text = $"{pcName}";
+        CPU.Text = $"CPU: {cpuName}";
+        Memory.Text = $"RAM: {ramCapacity}";
+    }
+
+    private string GetCPUName()
+    {
+        string cpuName = string.Empty;
+        using (var searcher = new ManagementObjectSearcher("select Name from Win32_Processor"))
+        {
+            foreach (var item in searcher.Get())
+            {
+                cpuName = item["Name"].ToString();
+                break;
+            }
+        }
+        return cpuName;
+    }
+
+    private string GetRAMCapacity()
+    {
+        double ramCapacityGB = 0;
+        using (var searcher = new ManagementObjectSearcher("SELECT Capacity FROM Win32_PhysicalMemory"))
+        {
+            foreach (var item in searcher.Get())
+            {
+                ramCapacityGB += Convert.ToDouble(item["Capacity"]) / (1024 * 1024 * 1024);
+            }
+        }
+        return $"{ramCapacityGB} GB";
+    }
+
+    // Constants for SystemParametersInfo function
+    private const int SPI_GETDESKWALLPAPER = 0x0073;
+    private const int MAX_PATH = 260;
+
+    // P/Invoke declaration for SystemParametersInfo function
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern int SystemParametersInfo(int uAction, int uParam, StringBuilder lpvParam, int fuWinIni);
+
+    // Method to retrieve the current user's wallpaper path
+    private string GetWallpaperPath()
+    {
+        StringBuilder wallpaperPath = new StringBuilder(MAX_PATH);
+        SystemParametersInfo(SPI_GETDESKWALLPAPER, MAX_PATH, wallpaperPath, 0);
+        return wallpaperPath.ToString();
+    }
+
+    public async void LoadWallpaper()
+    {
+        try
+        {
+            Wallpaper.Source = new BitmapImage(new Uri(GetWallpaperPath(), UriKind.RelativeOrAbsolute));
+        }
+        catch
+        {
+            await App.cpanelWin.ShowMessageDialogAsync("You need to run this app as administrator in order to retrieve the wallpaper.");
+        }
+    }
+
+    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if ((sender as ComboBox).SelectedIndex == 0 && (App.cpanelWin != null))
+        {
+            App.cpanelWin.AddressBox.Text = @"Control Panel";
+            App.cpanelWin.NavigateToPath(true);
+        }
+        if ((sender as ComboBox).SelectedIndex == 1 && (App.cpanelWin != null))
+        {
+            App.cpanelWin.AddressBox.Text = @"Control Panel";
+            App.cpanelWin.NavigateToPath();
+        }
+    }
+
+    private async void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        try
+        {
+            if ((NavigationViewItem)sender.SelectedItem == AppearanceItem)
+            {
+                App.cpanelWin.AddressBox.Text = @"Control Panel\Appearance and Personalization";
+                App.cpanelWin.NavigateToPath();
+            }
+            else if ((string)((NavigationViewItem)sender.SelectedItem).Tag is "SysAndSecurity")
+            {
+                App.cpanelWin.AddressBox.Text = @"Control Panel\System and Security";
+                App.cpanelWin.NavigateToPath();
+            }
+            else if ((string)((NavigationViewItem)sender.SelectedItem).Tag is "Programs" or "Uninstall a program")
+            {
+                await Launcher.LaunchUriAsync(new Uri("ms-settings:appsfeatures"));
+            }
+        }
+        catch (Exception ex)
+        {
+            if (App.cpanelWin != null) App.cpanelWin.Title = ex.Message;
+        }
+    }
+
+    private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+    {
+        App.cpanelWin.RootFrame.Navigate(typeof(AppearanceAndPersonalization), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+    }
+
+    private void SettingsCard_Click(object sender, RoutedEventArgs e)
+    {
+        var info = new ProcessStartInfo()
+        {
+            FileName = "powershell.exe",
+            Arguments = "Start-Process -FilePath \"C:\\Windows\\System32\\control.exe\"",
+            Verb = "runas",
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        var process = Process.Start(info);
+
+        App.cpanelWin.Close();
+    }
+
+    private void MenuFlyoutItem_Click_1(object sender, RoutedEventArgs e)
+    {
+        App.cpanelWin.RootFrame.Navigate(typeof(SystemAndSecurity), null, new Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+    }
+
+    private void SettingsCard_Click_1(object sender, RoutedEventArgs e)
+    {
+        // App.m_window = new MainWindow();
+        //App.m_window.Activate();
+        //App.cpanelWin.Close();
+    }
+}
