cec1883879026de301ec1016531634f7fb036e7a
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-12-31 21:04:14 +0200

    Code refactor

--- About/App.xaml.cs
+++ About/App.xaml.cs
@@ -1,33 +1,26 @@
 ﻿using Microsoft.UI.Xaml;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using WinUIEx;
 
 namespace Rebound.About;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
 public partial class App : Application
 {
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
+    public static WindowEx MainWindow { get; set; }
+
     public App()
     {
-        this.InitializeComponent();
+        InitializeComponent();
+        Current.UnhandledException += App_UnhandledException;
     }
 
-    /// <summary>
-    /// Invoked when the application is launched.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
-    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
     {
-        m_window = new MainWindow();
-        m_window.Activate();
+        e.Handled = true;
     }
 
-    private Window m_window;
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        MainWindow = new MainWindow();
+        MainWindow.Activate();
+    }
 }
