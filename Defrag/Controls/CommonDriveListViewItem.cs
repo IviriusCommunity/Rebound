cf43b1e7674788c40e8abed0d62316d24755dd58
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2025-01-16 23:34:56 +0200

    Code refactor

--- Defrag/Controls/CommonDriveListViewItem.cs
+++ Defrag/Controls/CommonDriveListViewItem.cs
@@ -1,20 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.UI.Dispatching;
-using Rebound.Defrag.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 #nullable enable
 
 namespace Rebound.Defrag.Controls;
 
 /// <summary>
-/// The drive items themselves seen in the main window
+/// The drive items themselves seen in the scheduled defrag window
 /// </summary>
 
 public partial class CommonDriveListViewItem : ObservableObject
@@ -32,16 +23,5 @@ public partial class CommonDriveListViewItem : ObservableObject
     public string? MediaType { get; set; }
 
     // Item selected value
-    public bool IsChecked
-    {
-        // Load the value from the settings
-        get => SettingsHelper.GetValue<bool>(
-            // Use the numerical representation of the drive letter to avoid conflicts
-            GenericHelpers.ConvertStringToNumericRepresentation(DrivePath));
-
-        // Set the value to the settings
-        set => SettingsHelper.SetValue(
-            // Use the numerical representation of the drive letter to avoid conflicts
-            GenericHelpers.ConvertStringToNumericRepresentation(DrivePath), value);
-    }
+    public bool IsChecked { get; set; }
 }
