c34e89a869007d4fea206d8c98adb9fd933d4805
Ivirius <zaitandreicristian@outlook.com>
2025-01-21 16:12:14 +0200

    Add helpers

---
+++ Helpers/Modding/IReboundApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace Rebound.Helpers.Modding;
+
+enum ReboundAppIntegrity
+{
+    Installed,
+    Corrupt,
+    NotInstalled
+}
+
+internal interface IReboundApp
+{
+    public void Install();
+
+    public void Uninstall();
+
+    public ReboundAppIntegrity GetIntegrity();
+}
