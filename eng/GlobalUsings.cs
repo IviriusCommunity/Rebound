d9329ea1b0ff09067bca88505fed4b39ffaba730
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-02-17 07:39:14 +0000

    `GlobalUsings.cs`

---
+++ eng/GlobalUsings.cs
@@ -0,0 +1,37 @@
+/*
+
+// System
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Linq;
+global using System.Threading.Tasks;
+
+// WinUI
+global using Microsoft;
+global using Microsoft.UI;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Data;
+global using Microsoft.UI.Xaml.Input;
+global using Microsoft.UI.Xaml.Media;
+global using Microsoft.UI.Xaml.Markup;
+global using Microsoft.UI.Xaml.Navigation;
+global using Microsoft.UI.Composition;
+global using Microsoft.UI.Xaml.Documents;
+global using Microsoft.UI.Xaml.Hosting;
+global using Microsoft.UI.Xaml.Media.Animation;
+global using Microsoft.UI.Xaml.Shapes;
+global using Microsoft.UI.Xaml.Controls.Primitives;
+
+global using Windows.UI;
+
+// CommunityToolkit
+// TODO: Add WCT packages
+
+// WinUIEx
+
+// global using WinUIEx;
+
+*/
