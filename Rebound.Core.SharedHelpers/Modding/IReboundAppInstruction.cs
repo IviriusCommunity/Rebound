6a22ea3d53546298df5b159edbb533fbd7966955
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2025-04-20 23:06:51 +0300

    Initial commit

---
+++ Rebound.Core.SharedHelpers/Modding/IReboundAppInstruction.cs
@@ -0,0 +1,25 @@
+﻿namespace Rebound.Helpers.Modding;
+
+public enum ReboundAppIntegrity
+{
+    Installed,
+    Corrupt,
+    NotInstalled
+}
+
+public enum InstallationTemplate
+{
+    Basic,
+    Recommended,
+    Complete,
+    Extras
+}
+
+public interface IReboundAppInstruction
+{
+    public void Apply();
+
+    public void Remove();
+
+    public bool IsApplied();
+}
---
+++ Rebound.Core.SharedHelpers/Services/ReboundAppService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+#nullable enable
+
+namespace Rebound.Helpers.Services;
+
+public partial class ReboundAppService
+{
+    public const string LEGACY_LAUNCH = "legacy";
+
+    public ReboundAppService(string legacyLaunchActionName)
+    {
+        AddLegacyAppLauncher(legacyLaunchActionName);
+    }
+
+    public static async void AddLegacyAppLauncher(string name)
+    {
+        // Get the app's jump list.
+        var jumpList = await Windows.UI.StartScreen.JumpList.LoadCurrentAsync();
+
+        // Disable the system-managed jump list group.
+        jumpList.SystemGroupKind = Windows.UI.StartScreen.JumpListSystemGroupKind.None;
+
+        // Remove any previously added custom jump list items.
+        jumpList.Items.Clear();
+
+        var item = Windows.UI.StartScreen.JumpListItem.CreateWithArguments(LEGACY_LAUNCH, name);
+        item.Logo = new Uri("ms-appx:///Assets/Computer disk.png");
+
+        jumpList.Items.Add(item);
+
+        // Save the changes to the app's jump list.
+        await jumpList.SaveAsync();
+    }
+}
