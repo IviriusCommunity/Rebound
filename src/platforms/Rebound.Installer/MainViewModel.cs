3a93a633f32a866ce5ba7351060e867e05b39d02
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2025-06-05 01:59:22 +0300

    Update

---
+++ src/platforms/Rebound.Installer/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Rebound.Installer;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial double Progress { get; set; } = 0;
+
+    [ObservableProperty]
+    public partial string Status { get; set; } = "Initializing...";
+
+    [ObservableProperty]
+    public partial string ErrorMessage { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial bool IsError { get; set; } = false;
+
+    [ObservableProperty]
+    public partial double Steps { get; set; } = 1;
+
+    public double GetAllItemsForFolder()
+    {
+        return 0;
+    }
+}
