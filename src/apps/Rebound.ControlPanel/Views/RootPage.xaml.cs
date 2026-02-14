6b04b0382bad59093dce0240e0d655e041fb14bd
IviriusMain <zaitandreicristian@outlook.com>
2026-02-14 05:06:16 +0200

    Add legacy launch button

--- src/apps/Rebound.ControlPanel/Views/RootPage.xaml.cs
+++ src/apps/Rebound.ControlPanel/Views/RootPage.xaml.cs
@@ -1,15 +1,28 @@
-using System.Xml.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Controls;
+using Rebound.Core.SystemInformation.Software;
+using Rebound.Core.UI;
+using System;
+using System.Xml.Linq;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 namespace Rebound.ControlPanel.Views;
 
+[INotifyPropertyChanged]
 public sealed partial class RootPage : Page
 {
+    [ObservableProperty]
+    private partial string UserPicturePath { get; set; }
+
     public RootPage()
     {
         InitializeComponent();
+        UIThreadQueue.QueueAction(() =>
+        {
+            UserPicturePath = UserInformation.GetUserPicturePath() ?? string.Empty;
+        });
         RootFrame.Navigate(typeof(HomePage));
     }
 
